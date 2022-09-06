a725c6da91f90ca133ef0dfcb86b33f925a94305
NecronicalPug <necronicalpug@gmail.com>
2022-09-07 00:56:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#    Copyright (C) 2022 Mateusz Drozda, necronicalpug@gmail.com
+#    Copyright (C) 2022 NecronicalPug, necronicalpug@gmail.com
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
