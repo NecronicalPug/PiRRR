45964a860fa6bb121ec68ae357cd18b0cbea117a
NecronicalPug <necronicalpug@gmail.com>
2023-12-03 01:12:54 +0000

    Licence update

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-#    Copyright (C) 2022 NecronicalPug, necronicalpug@gmail.com
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+Copyright (C) 2022 NecronicalPug, necronicalpug@gmail.com
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import os
 import sys
@@ -511,7 +513,7 @@ class iRDataClientButBetter:
         try:
             initial_hash = hashlib.sha256((password + username.lower()).encode('utf-8')).digest()
             self.encoded_password = base64.b64encode(initial_hash).decode('utf-8')
-        except Exception as ex:
+        except Exception:
             self.encoded_password = None
 
     def login(self):
